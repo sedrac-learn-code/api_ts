e5ae7b2768d9b59cbb4e661881bdb8340e99677b
sedrac-slc <slcsedrac@gmail.com>
2025-09-25 13:29:21 +0100

    create test findFirstName, findFirstEmail

--- test/src/repository/customer.repository.spec.ts
+++ test/src/repository/customer.repository.spec.ts
@@ -6,14 +6,20 @@ import { CustomerFaker } from '../faker/customer.faker';
 describe('Customer Repository', () => {
   const repository = new CustomerRepository(new PrismaClient());
 
-  test('findMany: Get all customers', async ()  => {
+  test('findMany_WhenSuccessfull: Get all customers', async ()  => {
     await repository.create(CustomerFaker.generate());
     const items = await repository.findMany();
     expect(items.length).toBeGreaterThan(0);
     expect(items).toBeInstanceOf(Array);
   });
 
-  test('create: Register customer', async ()  => {
+  test('findById_WhenSuccessfull: Get customer by id', async ()  => {
+    const item = await repository.create(CustomerFaker.generate());
+    const items = await repository.findById(item.id);
+    expect(items).not.toBeNull();
+  });
+
+  test('create_WhenSuccessfull: Register customer', async ()  => {
     const item = await repository.create(CustomerFaker.generate());
     expect(item).toHaveProperty('id');
     expect(item).toHaveProperty('name');
@@ -21,7 +27,7 @@ describe('Customer Repository', () => {
     expect(item.id).toBeGreaterThan(0);
   });  
 
-  test('update: Update customer', async ()  => {
+  test('update_WhenSuccessfull: Update customer', async ()  => {
     const item = await repository.create(CustomerFaker.generate());
     const customer = await repository.update(item.id, CustomerFaker.generate());
     expect(customer.id).toEqual(item.id);
@@ -29,11 +35,23 @@ describe('Customer Repository', () => {
     expect(customer.email).not.toEqual(item.email);
   });
 
-  test('delete: Delete customer', async ()  => {
+  test('delete_WhenSuccessfull: Delete customer', async ()  => {
     const item = await repository.create(CustomerFaker.generate());
     await repository.delete(item.id);
     await expect(await repository.findById(item.id)).toBeNull();
-  });     
+  });   
+  
+  test('findFirstName_WhenSuccessfull: Get customer by name', async ()  => {
+    const item = await repository.create(CustomerFaker.generate());
+    const customer = await repository.findFirstName(item.name);
+    expect(customer).not.toBeNull();
+  });
+  
+  test('findFirstEmail_WhenSuccessfull: Get customer by email', async ()  => {
+    const item = await repository.create(CustomerFaker.generate());
+    const customer = await repository.findFirstEmail(item.email);
+    expect(customer).not.toBeNull();
+  });  
   
 });
 
