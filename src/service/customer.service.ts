2cffa5d49e08b42f6eab94c2b5fa4090ea63a2cb
sedrac-slc <slcsedrac@gmail.com>
2025-09-26 10:30:07 +0100

    create validation CustomDTO in service(create, update) methods

--- src/service/customer.service.ts
+++ src/service/customer.service.ts
@@ -1,6 +1,7 @@
 import { Customer } from "../generated/prisma";
 import { CustomerDTO } from "../dto/customer.dto";
 import { CustomerRepository } from "../repository/customer.repository";
+import { ValidationException } from "../exception/validation.exception";
 
 export class CustomerService {
     private customRepository;
@@ -14,15 +15,22 @@ export class CustomerService {
     }
 
     async create(data: CustomerDTO): Promise<Customer> {
+        this.validationCustomerDTO(data);
         return this.customRepository.create(data);
     }
 
     async update(id: number, data: CustomerDTO): Promise<Customer> {
+        this.validationCustomerDTO(data);
         return this.customRepository.update(id, data);
     }
 
     async delete(id: number): Promise<void> {
         await this.customRepository.delete(id);
+    }
+
+    async validationCustomerDTO(data: CustomerDTO): Promise<void> {
+        if(await this.customRepository.findFirstEmail(data.email)) throw new ValidationException('Email exists');
+        if(await this.customRepository.findFirstName(data.name)) throw new ValidationException('Name exists');
     }
   
 }
